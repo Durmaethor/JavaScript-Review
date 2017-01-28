50eb18673eb9c188faeb2def2f3f326b2cb68cac
Ryan Kramer <rkramer613@gmail.com>
2017-01-28 10:26:18 -0700

    First to Second

--- arrayReview.js
+++ arrayReview.js
@@ -62,12 +62,17 @@ alert(randomNumber(randomArray));
 
 //Create a copy of first and save it into second. Then, add 6 and 7 to the end of second. When you run this, first should be just [1,2,3,4,5] and second will be [1,2,3,4,5,6,7] if you created your copy correctly.
 var first = [1,2,3,4,5];
-var second;
+var second = [];
 
-  //code here
+function newSecond(first) {
+  var second = first.slice(0);
+    second.push(6,7);
+    return second;
+}
 
-alert(first) //[1,2,3,4,5];
-alert(second) //[1,2,3,4,5,6,7];
+alert(newSecond(first));
+
+alert(first);
 
 
 
